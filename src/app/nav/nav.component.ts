620aa9e646862dcb8be29a9508ea929b1a39f443
IsaiCordero <isaicordero03@gmail.com>
2024-04-23 10:25:11 +0100

    landing page, footer, nav y header

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.css'
+})
+export class NavComponent {
+  constructor(private router: Router) {}
+
+  redirectToProfile() {
+    this.router.navigate(['../../profile']);
+  }
+
+}
